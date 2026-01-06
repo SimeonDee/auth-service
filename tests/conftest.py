8cea4be4a6b094042a2734e438439dcbd8e27992
SimeonDee <adeyemi.adedoyin.simeon@gmail.com>
2026-01-07 00:06:11 +0100

    resolved CI test failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from httpx import AsyncClient, ASGITransport
 from asgi_lifespan import LifespanManager
 
-from app.main import app
+from ..app.main import app
 from ..app.db.session import Base, get_db
 
 
